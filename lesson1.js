5552436439a6e15795a179544106ee8b8aaf4f6f
Axeliriya <axeliriya@mail.ru>
2020-04-11 00:28:20 +0200

    Закончено задание 1

--- lesson1.js
+++ lesson1.js
@@ -1,23 +1,32 @@
 'use strict'
 
-let money = prompt('Ваш бюджет на месяц?', ''),
-    time = prompt ('Введите дату в формате YYYY-MM-DD', '');
+// let money = prompt('Ваш бюджет на месяц?', ''),
+//     time = prompt ('Введите дату в формате YYYY-MM-DD', '');
 
-let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    saving: false
-};
-let q1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    q2 = prompt('Во сколько обойдется?', ''),
-    q3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
-    q4 = prompt('Во сколько обойдется?', '');
+// let appData = {
+//     budget: money,
+//     timeData: time,
+//     expenses: {},
+//     optionalExpenses: {},
+//     income: [],
+//     saving: false
+// };
+// let q1 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//     q2 = prompt('Во сколько обойдется?', ''),
+//     q3 = prompt('Введите обязательную статью расходов в этом месяце', ''),
+//     q4 = prompt('Во сколько обойдется?', '');
 
-    appData.expenses[q1] = q2;
-    appData.expenses[q3] = q4;
+//     appData.expenses[q1] = q2;
+//     appData.expenses[q3] = q4;
 
-alert(appData.budget / 30);
-console.log((appData.budget) - (appData.expenses[q1]));
+// alert(appData.budget / 30);
+// console.log((appData.budget) - (appData.expenses[q1]));
+
+let y = "ёжик",
+    x = "яблоко";
+    if (y > x) {
+        console.log("Yes");
+    } else {
+        console.log("No");
+    }
+    console.log(0 || "" || 2 || undefined || true || falsе);
